f4fe7824d7bafaff7ab45996f4be8ae351699e62
sintiaturnip <sintiaturnip3@gmail.com>
2026-05-18 11:08:47 +0800

    integrasi product route di app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
 const sequelize = require('./config/db');
-
 const db = require('./models');
+
+// 1. Import routes yang sudah ada
+const userRoutes = require('./routes/user.route');
+// Tambahkan import product route sesuai gambar
+const productRoutes = require('./routes/product.route');
 
 const app = express();
 
@@ -10,37 +13,28 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('API aktif!'));
 
-// tambahkan routing kamu dibawah sini
+// --- Routing Integrasi ---
+
+app.use('/api/users', userRoutes);
+// 2. Tambahkan baris ini untuk product
+app.use('/api/products', productRoutes);
+
+// --------------------------
 
 // Koneksi ke database
-
 sequelize.authenticate()
-
   .then(() => {
-
     console.log('✅ Terkoneksi ke MySQL');
-
     return db.sequelize.sync({ alter: true });
-
   })
-
   .then(() => {
-
     console.log('✅ Sinkronisasi selesai');
-
   })
-
   .catch((err) => {
-
     console.error('❌ Gagal konek:', err);
-
   });
 
 // Jalankan server
-
 app.listen(3000, () => {
-
   console.log('🚀 Server jalan di http://localhost:3000');
-
 });
-
