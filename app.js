a025dcae6acfd98fae9e70d96a5e2e7186fc6a02
sintiaturnip <sintiaturnip3@gmail.com>
2026-05-18 11:04:14 +0800

    menghubungkan user routes di app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const sequelize = require('./config/db');
-
 const db = require('./models');
+
+// 1. Tambahkan import route di sini
+const userRoutes = require('./routes/user.route');
 
 const app = express();
 
@@ -10,37 +11,27 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('API aktif!'));
 
-// tambahkan routing kamu dibawah sini
+// --- Tambahkan routing kamu dibawah sini ---
+
+// 2. Hubungkan rute user ke aplikasi
+app.use('/api/users', userRoutes);
+
+// -------------------------------------------
 
 // Koneksi ke database
-
 sequelize.authenticate()
-
   .then(() => {
-
     console.log('✅ Terkoneksi ke MySQL');
-
     return db.sequelize.sync({ alter: true });
-
   })
-
   .then(() => {
-
     console.log('✅ Sinkronisasi selesai');
-
   })
-
   .catch((err) => {
-
     console.error('❌ Gagal konek:', err);
-
   });
 
 // Jalankan server
-
 app.listen(3000, () => {
-
   console.log('🚀 Server jalan di http://localhost:3000');
-
 });
-
